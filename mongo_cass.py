a3ebb94bff23a0bbe95ba20fac7e0d20151c5d79
cloubrain <cloubrain@gmail.com>
2014-04-07 15:58:18 -0700

    Update mongo_cass.py

--- mongo_cass.py
+++ mongo_cass.py
@@ -23,16 +23,12 @@ cass = cbcassandra.CBcassandra(chost, keyspace)
 cass.setup_schema()
 
 # MongoBD
-dcname = "dkan-cluster-1-dc-19"  # 21Vms 26.1M. 100K takes 
-dcname = "dkan-cluster-1-da-96"  # 30VMs 27.5M. 46K takes 20hrs.
-dcname = "dkan-cluster-1-da-20"  # 46VMs 55.8M. 100K takes 
-dcname = "dkan-standalone-esxi"  # 18VMs 17.7M. 100K takes 
-dcname = "dkan-cluster-2-DC"     # 11VMs 10.0M. 100K takes
-  # 3000/800: 10min
-  # 3000/400: 15min
-  # 2000/400: 9min
-  # 2000/200: 10min
-  # 2000/400(batch): 
+dcname = "dkan-cluster-1-dc-19"  # 21Vms 26.1M. 
+dcname = "dkan-cluster-1-da-96"  # 30VMs 27.5M. 
+dcname = "dkan-cluster-1-da-20"  # 46VMs 55.8M. 
+dcname = "dkan-standalone-esxi"  # 18VMs 17.7M. 
+dcname = "dkan-cluster-2-DC"     # 11VMs 10.0M. 
+
 cbmongo = cbmongo.CBmongo(dcname)  #init CBmongo
 #cbdb.testdb()  # tests functions
 #cbdb.getStats()
@@ -50,8 +46,6 @@ datanum = 2000
 datachunk = 200 # chunk of 200-400 (x16 perfmetrics) is efficient
 place = 0
 
-# vms = cbmongo.getData(dcname, 0, 10)
-
 print ' [*] Migrating data from MongoDB to Cassandra'
 
 while place < datanum:
@@ -64,19 +58,12 @@ while place < datanum:
         perfdata["vmname"] = vm
         for st in vmdata[vm]:
             perfdata["time_current"] = st["time_current"]
-            #batchstr = ""
-            #cur.execute("BEGIN BATCH ")
             for perf in st["perfMetrics"]:
                 perfdata["pname"] = perf['perfkey']
                 v = perf['v']
                 perfdata["pvalue"] = int(float(v))
-                #cass.store(perfdata)
-                #insertstr = cass.return_insert(perfdata)
-                #batchstr = batchstr + insertstr
                 cass.insert_datasimple(cur, perfdata)
-            #cass.batch_insert(cur, batchstr)
-            #cur.execute("APPLY BATCH;")
-    
+
     cur.shutdown()
     time.sleep(1)
 
